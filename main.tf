b9d47eed2c314bacee687116d214b3456b81b1d3
Chanu <kollichanu@gmail.com>
2026-02-25 20:46:42 -0600

    added infra code

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  backend "s3" {
+	bucket = "terraform-state-chanubucket"
+	key    = "github-actions-runner/terraform.tfstate"
+	region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "instances" {
+  ami           = var.ami
+  instance_type = "t3.small"
+  vpc_security_group_ids = var.vpc_security_group_ids
+  iam_instance_profile = "arn:aws:iam::444206648334:role/workstation-role"
+
+  tags = {
+	Name = "github-actions-runner"
+  }
+
+}
+
+resource "null_resource" "ansible" {
+
+  provisioner "remote-exec" {
+	connection {
+	  type     = "ssh"
+	  user     = "ec2-user"
+	  password = "DevOps321"
+	  host     = aws_instance.instances.private_ip
+	}
+
+	inline = [
+	  "sudo dnf install python3.13-pip -y",
+	  "sudo pip3.11 install ansible",
+	  "ansible-pull -i localhost, -U https://github.com/chanu-devops/github-actions-runners-configuration.git runner.yml -e TOKEN=${var.TOKEN}"
+	]
+
+  }
+
+}
+
+variable "TOKEN" {}
+variable "ami" {
+  default = "ami-0220d79f3f480ecf5"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-099eff6e665cecd4a"]
+}
